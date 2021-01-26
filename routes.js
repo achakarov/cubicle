90a3d9d51133ea545cddb04b8d9a1cad463f33b0
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-26 19:46:51 +0200

    Change About to Home controller

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const productController = require('./controllers/productController');
-const aboutController = require('./controllers/aboutController');
+const homeController = require('./controllers/homeController');
 
 const router = Router();
 
-router.use('/', productController);
-router.use('/about', aboutController);
+router.use('/products', productController);
+router.use('/', homeController);
 router.get('*', (req, res) => {
     res.render('404');
 });
