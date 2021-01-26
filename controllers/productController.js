9fe848e22894cdbeb28fcf8ca52a97f2e2f118b3
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-26 20:17:27 +0200

    Re-direct to home page

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,6 +11,8 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res)=> {
     console.log(req.body);
+
+    res.redirect('/products');
 }); 
 
 router.get('/details/:productId', (req, res) => {
