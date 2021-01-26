d6d779e4115ea0a7a5dedc44e0133d2817197538
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-26 19:37:58 +0200

    Add layout to page

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars');
 
 function setupExpress (app){
     app.engine('hbs', handlebars({
-        extname: 'hbs'
+        extname: 'hbs',
     }));
     
     app.set('view engine', 'hbs');
