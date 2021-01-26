3da43db0a8866ab2aa8f5d2437f36aaa77afa20a
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-26 21:46:56 +0200

    Search fully implemented

--- services/productService.js
+++ services/productService.js
@@ -10,6 +10,15 @@ function getAll(query) {
     if (query.search) {
         result = result.filter(x => x.name.toLowerCase().includes(query.search));
     }
+
+    if (query.from) {
+        result = result.filter(x => Number(x.difficultyLevel) >= query.from);
+    }
+
+    if (query.to) {
+        result = result.filter(x => Number(x.difficultyLevel) <= query.to);
+    }
+    
     return result;
 }
 
