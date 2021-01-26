c07a9359894e963b1040ba11f26f45ffd6cd9f73
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-26 20:44:40 +0200

    Created db logic

--- services/productService.js
+++ services/productService.js
@@ -1,7 +1,13 @@
 const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
+const fs = require('fs');
+let productsData = require('../config/products.json');
 
-function createProduct(data) {
+function getAll() {
+    return productsData;
+}
+
+function create(data) {
     let cube = new Cube(
         uniqid(),
         data.name,
@@ -10,9 +16,16 @@ function createProduct(data) {
         data.difficultyLevel
     );
 
-    
+    productsData.push(cube);
+
+    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
 }
 
 module.exports = {
-    createProduct
+    create
 }
