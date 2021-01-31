df562de0c6fbd7bc37f4e3962772b93c948886f9
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-31 17:07:29 +0200

    created Cube schema

--- models/Cube.js
+++ models/Cube.js
@@ -1,6 +1,29 @@
-const Model = require('./Model');
+const mongoose = require('mongoose');
 
-class Cube extends Model {
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    }
+});
+
+class Cube {
     constructor(id, name, description, imageUrl, difficultyLevel) {
         this.id = id;
         this.name = name;
