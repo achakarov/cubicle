aec6b344b1f0527efa5adadf13f143fdac537c59
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-31 16:36:32 +0200

    Created common Model and Cube extends it

--- models/Cube.js
+++ models/Cube.js
@@ -1,31 +1,12 @@
-const fs = require('fs/promises');
-const path = require('path');
-let productsDb = require('../config/products.json');
+const Model = require('./Model');
 
-class Cube {
+class Cube extends Model {
     constructor(id, name, description, imageUrl, difficultyLevel) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficultyLevel = difficultyLevel;
-    }
-
-    save() {
-        productsDb.push(this);
-
-        return fs.writeFile(
-            path.join(__dirname + '/../config/products.json'),
-            JSON.stringify(productsDb)
-        );
-    }
-
-    static getAll() {
-        return productsDb;
-    }
-    
-    static getOne(id) {
-        return productsDb.find(x => x.id === id);
     }
 }
 
