6d0257cd73476c30d274e95ad94b6122402f5808
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-31 18:55:39 +0200

    create accessory is working

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const productController = require('./controllers/productController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 
 const router = Router();
 
 router.use('/products', productController);
+router.use('/accessories', accessoryController);
 router.use('/', homeController);
 router.get('*', (req, res) => {
     res.render('404');
