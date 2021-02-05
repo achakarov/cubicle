e863ee7c60617a56264c4abf9ed07844fcd47125
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-05 18:37:56 +0200

    created register and login page

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const productController = require('./controllers/productController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 const router = Router();
 
 router.use('/products', productController);
+router.use('/auth', authController); 
 router.use('/accessories', accessoryController);
 router.use('/', homeController);
 router.get('*', (req, res) => {
