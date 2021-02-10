07e88fb410a2cf9a8c33cd5d3c62086b1703cd35
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-10 20:37:07 +0200

    Added validations to the Cube and Accessory models

--- models/Accessory.js
+++ models/Accessory.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
+const ENGLISH_ALPHANUMERIC_PATTERN = /^[a-zA-Z0-9\s]+$/;
 
 const accesorySchema = new mongoose.Schema({
     id: mongoose.Types.ObjectId,
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => `${props.value}is invalid accessory name. It should contain only English letters, digits and whitespaces!`
+        }
     },
     description: {
         type: String,
         required: true,
-        maxlength: 100
+        minlength: 20,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => 'Description should be at least 20 characters long'
+        }
     },
     imageUrl: {
         type: String,
--- models/Cube.js
+++ models/Cube.js
@@ -1,14 +1,28 @@
 const mongoose = require('mongoose');
+const ENGLISH_ALPHANUMERIC_PATTERN = /^[a-zA-Z0-9\s]+$/;
 
 const cubeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => `${props.value}is invalid cube name. It should contain only English letters, digits and whitespaces!`
+        }
     },
     description: {
         type: String,
         required: true,
-        maxlength: 100
+        minlength: 20,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => 'Description should be at least 20 characters long'
+        }
     },
     imageUrl: {
         type: String,
