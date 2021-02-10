b41afd971819115f45cb993339cbf432082e152b
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-10 18:22:57 +0200

    Removed error from template

--- services/auhtService.js
+++ services/auhtService.js
@@ -1,7 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const saltRounds = 10;
 const secret = 'megaSecret';
 
 const register = async ({ username, password }) => {
